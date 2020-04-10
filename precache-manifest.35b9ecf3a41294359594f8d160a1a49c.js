acef50a8bb6ed4ef6934c4c98cb31db31d1ea52f
Ryne Dionisio <ryne.dionisio@gmail.com>
2020-04-10 18:50:01 -0500

    Updates

---
+++ precache-manifest.35b9ecf3a41294359594f8d160a1a49c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bebe4b60ea78f46f9025d7a82c5637",
+    "url": "/jackbox-picker/index.html"
+  },
+  {
+    "revision": "26fb4f77d4956dea56ce",
+    "url": "/jackbox-picker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8d383ba31f23d5068d36",
+    "url": "/jackbox-picker/static/css/main.d388ae4b.chunk.css"
+  },
+  {
+    "revision": "26fb4f77d4956dea56ce",
+    "url": "/jackbox-picker/static/js/2.3c06342e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jackbox-picker/static/js/2.3c06342e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d383ba31f23d5068d36",
+    "url": "/jackbox-picker/static/js/main.4d593d28.chunk.js"
+  },
+  {
+    "revision": "12c6ece6bb5a63e19546",
+    "url": "/jackbox-picker/static/js/runtime-main.6a957121.js"
+  },
+  {
+    "revision": "340ae1f732283231ba34e46eed24f697",
+    "url": "/jackbox-picker/static/media/civic.340ae1f7.jpg"
+  },
+  {
+    "revision": "3b85f4d106c6502c13821e3fc1af4a1d",
+    "url": "/jackbox-picker/static/media/drawful2.3b85f4d1.jpg"
+  },
+  {
+    "revision": "14dcba03cd82641f4b4b8a203c732aab",
+    "url": "/jackbox-picker/static/media/fibbage3.14dcba03.png"
+  },
+  {
+    "revision": "1418e13233461dc273c4230c9e6b160f",
+    "url": "/jackbox-picker/static/media/guess.1418e132.jpg"
+  },
+  {
+    "revision": "b81f888b0b81f9094847d91527aba403",
+    "url": "/jackbox-picker/static/media/joke.b81f888b.jpg"
+  },
+  {
+    "revision": "e3291d79b635b7fb8f15292890fab1a6",
+    "url": "/jackbox-picker/static/media/mad.e3291d79.jpg"
+  },
+  {
+    "revision": "dc65d347abf82ce8babda1afe78e2ef5",
+    "url": "/jackbox-picker/static/media/monster.dc65d347.jpg"
+  },
+  {
+    "revision": "1fbdaae957ad36acbdaa009ae30f8c1b",
+    "url": "/jackbox-picker/static/media/patently.1fbdaae9.png"
+  },
+  {
+    "revision": "ae47f7d6ddd51e6d7c2686087d8fe8c5",
+    "url": "/jackbox-picker/static/media/teeko.ae47f7d6.jpg"
+  },
+  {
+    "revision": "0c63bcd386dc94c825337a2070dcc049",
+    "url": "/jackbox-picker/static/media/ydkjfs.0c63bcd3.png"
+  }
+]);
